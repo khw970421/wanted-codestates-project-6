7fd13b002677730b2c29dce63697f72d39bb09b0
khw970421 <khw970421@kakao.com>
2022-03-21 16:17:00 +0900

    🎉 begin : router 틀 잡기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Select from './pages/Select';
+import Schedule from './pages/Schedule';
+import Address from './pages/Address';
+import Result from './pages/Result';
+import Submit from './pages/Submit';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/care/select" element={<Select />} />
+        <Route path="/care/schedule" element={<Schedule />} />
+        <Route path="/care/address" element={<Address />} />
+        <Route path="/care/result" element={<Result />} />
+        <Route path="/care/submit" element={<Submit />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
