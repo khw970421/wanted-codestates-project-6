983265d8aa2bf20a72e75332888cc4b7b4bc16bf
khw970421 <khw970421@kakao.com>
2022-03-21 17:15:15 +0900

    ✨ feat : Main페이지 틀 구현

---
+++ src/components/Button.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+const Button = ({
+  text = '신청하기',
+  width,
+  height,
+  backgroundColor,
+  color,
+  fontSize,
+}) => {
+  return (
+    <CustomButton
+      width={width}
+      height={height}
+      backgroundColor={backgroundColor}
+      color={color}
+      fontSize={fontSize}
+    >
+      {text}
+    </CustomButton>
+  );
+};
+
+const CustomButton = styled.button`
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
+  border-radius: ${({ height }) => `${height}px`};
+  border: 0px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ color }) => color};
+  font-size: ${({ fontSize }) => `${fontSize}px`};
+  font-weight: bold;
+`;
+
+Button.propTypes = {
+  text: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  backgroundColor: PropTypes.string,
+  color: PropTypes.string,
+  fontSize: PropTypes.number,
+};
+
+export default Button;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
-
+import Text from '../components/Text';
+import Button from '../components/Button';
+import styled from '@emotion/styled';
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <Real>
+      <MainContainer>
+        <Text text={'어르신 돌보미'} fontSize={32} width={250} height={52} />
+        <Text
+          text={'케어코디'}
+          fontSize={32}
+          bold={'bold'}
+          width={250}
+          height={52}
+          color={'#ff8450'}
+        />
+        <Button
+          width={184}
+          height={56}
+          backgroundColor={'#FF8450'}
+          color={'#ffffff'}
+          fontSize={18}
+        />
+      </MainContainer>
+    </Real>
+  );
 };
+
+const Real = styled.div`
+  background-color: #f6f4fc;
+`;
+
+const MainContainer = styled.div`
+  margin: 0 auto;
+  width: 360px;
+  height: 812px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+`;
 
 export default Main;
