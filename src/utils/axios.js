09a73a27768c7ed01768b9e659f57a75e421e7f6
khw970421 <khw970421@kakao.com>
2022-03-25 13:23:54 +0900

    🐛 fix : netlify 배포 수정 코드 작성

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
 const getRepository = async (target, page) => {
+  const params = {
+    keyword: target,
+    currentPage: page,
+    countPerPage: 5,
+    confmKey: process.env.REACT_APP_SERVICE_KEY,
+    resultType: 'json',
+  };
+
   try {
-    const response = await axios.get(
-      `/api/addrlink/addrLinkApiJsonp.do?keyword=${target}&currentPage=${page}&countPerPage=5&resultType=JSON&confmKey=${process.env.REACT_APP_SERVICE_KEY}`,
-    );
+    const response = await axios.get(`/api/addrlink/addrLinkApiJsonp.do`, {
+      params,
+    });
     const data = response.data;
     return data;
   } catch (error) {
